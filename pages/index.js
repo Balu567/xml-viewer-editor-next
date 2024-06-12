e8e4ea5cf4cfbd7590680eed224e380ce272e201
Balu567 <balarajumalla.9@gmail.com>
2024-06-13 00:45:16 +1000

    render xml to buttons

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,73 @@
 import React, { useState } from 'react';
-import { xml2json, json2xml } from 'xml-js';
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-xml';
-import 'ace-builds/src-noconflict/theme-github';
+import { xml2json } from 'xml-js';
+import pd from 'pretty-data';
+import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.css';
 
+const MonacoEditor = dynamic(import('@monaco-editor/react'), { ssr: false });
+
+const parseXmlToJson = (xml) => {
+  const json = JSON.parse(xml2json(xml, { compact: true, spaces: 2 }));
+  return json;
+};
+
+const renderButtons = (json) => {
+  if (!json || typeof json !== 'object') return null;
+
+  const renderButtonFromNode = (key, value) => {
+    if (value._text) {
+      return (
+        <button key={key} className={styles.button}>
+          {`${key}: ${value._text}`}
+        </button>
+      );
+    } else if (typeof value === 'object') {
+      return Object.keys(value).map((nestedKey) =>
+        renderButtonFromNode(nestedKey, value[nestedKey])
+      );
+    }
+    return null;
+  };
+
+  return Object.keys(json).map((key) => renderButtonFromNode(key, json[key]));
+};
+
 const Home = () => {
-  const [xml, setXml] = useState('<root><child>Hello World</child></root>');
-  const [json, setJson] = useState(xml2json(xml, { compact: true, spaces: 2 }));
+  const initialXml = '<root><child>Hello World</child><child>Another Child</child></root>';
+  const [xml, setXml] = useState(pd.pd.xml(initialXml));
+  const [json, setJson] = useState(parseXmlToJson(initialXml));
 
   const handleXmlChange = (newXml) => {
-    setXml(newXml);
     try {
-      const newJson = xml2json(newXml, { compact: true, spaces: 2 });
+      const formattedXml = pd.pd.xml(newXml);
+      setXml(formattedXml);
+      const newJson = parseXmlToJson(formattedXml);
       setJson(newJson);
     } catch (e) {
       console.error('Invalid XML:', e);
     }
   };
 
-
-
   return (
     <div className={styles.container}>
-      <h1>XML Viewer and Editor</h1>
+      <h1>XML Editor</h1>
       <div className={styles.editor}>
         <h2>XML Editor</h2>
-        <AceEditor
-          mode="xml"
-          theme="github"
-          onChange={handleXmlChange}
+        <MonacoEditor
+          height="400px"
+          defaultLanguage="xml"
           value={xml}
-          name="xmlEditor"
-          editorProps={{ $blockScrolling: true }}
-          width="100%"
-          height="200px"
+          onChange={handleXmlChange}
+          theme="vs-dark"
+          options={{ automaticLayout: true }}
         />
       </div>
-
+      <div className={styles.viewer}>
+        <h2>Rendered Components</h2>
+        <div className={styles.buttonsContainer}>
+          {renderButtons(json)}
+        </div>
+      </div>
     </div>
   );
 };
